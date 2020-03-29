007840e4dd9d4b0616c03fe2b22f913ca2138a2b
Moises Rivera <moisesrivera796@gmail.com>
2020-03-29 12:13:18 -0600

    Material y size

--- view/material.php
+++ view/material.php
@@ -1,13 +1,12 @@
 
-<title>Colores</title>
+<title>Material</title>
 <div class="animated slideInLeft">
-<a class=" btn z-depth-5   modal-trigger" id="btng" href="#modal1"><i class="material-icons right">color_lens</i>Agregar colores</a>
+<a class=" btn z-depth-5   modal-trigger" id="btng" href="#modal1"><i class="material-icons right">color_lens</i>Agregar material</a>
 
       <div class="tb">
-                    <table id="tbcolor" class="table table-striped table-bordered table-hover text-center "  cellspacing="0" width="100%">
+                    <table id="tbmaterial" class="table table-striped table-bordered table-hover text-center "  cellspacing="0" width="100%">
                             <thead >
                             <tr>
-                              <th class="text-center">Color</th>
                               <th class="text-center">Nombre</th>
                               <th class="text-center">Acciones</th>
                             </tr>
@@ -15,7 +14,6 @@
                             <tbody ></tbody>
                              <tfoot>
                             <tr>
-                              <th class="text-center">Color</th>
                               <th class="text-center">Nombre</th>
                               <th class="text-center">Acciones</th>
                             </tr>
@@ -27,18 +25,14 @@
   <!-- Modal Structure -->
   <div id="modal1" class="modal modal-fixed-footer peque">
     <div class="modal-content">
-      <h4>Colores</h4>
+      <h4>Material.</h4>
       <div class="row">
-        <form class="col s12 center-align" id="formcolor" name="formcolor" method="post">
+        <form class="col s12 center-align" id="formmaterial" name="formmaterial" method="post">
         <div class="row">
-           <div class="input-field col s12">
-           <input type="color" name="pcolor" id="pcolor"><input type="hidden" name="txtcode" title="Selecciona un color (*)" id="txtcode">
-            <span class="" id="txtcodeerror">Selecciona un color</span>
-          </div>
           <div class="input-field col s12">
-            <input type="text" name="txtcolor" title="Nombre (*)" id="txtcolor">
-            <label for="txtcolor">Color</label>
-            <span class="" id="txtcolorerror"></span>
+            <input type="text" name="txtname" title="Nombre (*)" id="txtname">
+            <label for="txtname">Nombre</label>
+            <span class="" id="txtnameerror"></span>
           </div>
         </div>
         <button type="submit" class=" btn">Guardar <i class="material-icons">save</i></button>
@@ -53,20 +47,16 @@
 
       <div id="modal2" class="modal modal-fixed-footer peque">
       <div class="modal-content">
-        <h4>Editar Colores</h4>
+        <h4>Editar Material.</h4>
         <div class="row">
-          <form class="col s12 center-align" name="formcolore" id="formcolore" method="post">
-           <div class="input-field col s12">
-           <input type="color" name="pcolore" id="pcolore"><input type="hidden" name="txtcodee" title="Selecciona un color" id="txtcodee">
-            <span class="" id="txtcodeeerror">Selecciona un color</span>
-          </div>
+          <form class="col s12 center-align" name="formmateriale" id="formmateriale" method="post">
+
           <div class="row">
-            <input type="hidden" name="colorid" title="nombre" id="colorid">
+            <input type="hidden" name="materialid" title="nombre" id="materialid">
             
             <div class="input-field col s12">
-              
-              <input  id="colore" name="colore" title="Nombre" type="text">
-              <span class="" id="txtcoloreerror">Nombre</span>
+              <input  id="txtnamee" name="txtnamee" title="Nombre" type="text">
+              <span class="" id="txtnameerrore">Nombre</span>
             </div>
           
 
@@ -83,4 +73,4 @@
 
 
 <?php require 'blocks/footer.php'; ?>
-    <script src="<?php echo SERVERURL; ?>/jsproject/color.js"></script>
+    <script src="<?php echo SERVERURL; ?>/jsproject/material.js"></script>
