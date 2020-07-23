01a6a5868aa464d0dbe345defe45644ad1f4e22a
Moises Rivera <moisesrivera796@gmail.com>
2020-07-22 19:37:16 -0600

    cart beta 03

--- store/js/shoppingdetails.js
+++ store/js/shoppingdetails.js
@@ -19,7 +19,7 @@ function consultcar(id){
                 var cont=0, total=0;
                 for (var i = 0; i < respu.length; i++) {
                 	cont++;
-                	    html+='<li class="collection-item"><div class="row"><div class="col s12 m12 l1 center-align"><a href="#!" style="margin-top:2rem;" class="btn red"><i class="material-icons">remove_shopping_cart</i></a></div> <div class="col s12 m12 l2 center-align">';
+                	    html+='<li class="collection-item"><div class="row"><div class="col s12 m12 l1 center-align"><a href="#!" style="margin-top:2rem;" class="btn red" onClick="deletefromcar('+i+','+String("'"+respu[i].id_shp_c+"'")+','+String("'"+respu[i].id_prices+"'")+','+String("'"+respu[i].id_mat+"'")+','+String("'"+respu[i].id_color+"'")+','+String("'"+respu[i].id_size+"'")+');"><i class="material-icons">remove_shopping_cart</i></a>  <br><br></div> <div class="col s12 m12 l2 center-align">';
 					      html+='<img src="../view/imgdetails/'+respu[i].img+'" style="height:100px; width:100;" ></div>';
 					      html+='<div class="col s12 m12 l3 center-align"><h5>'+respu[i].name_pro+'</h5>';
 					      
@@ -31,10 +31,10 @@ function consultcar(id){
                           html+='</div>';
 					       html+='<div class="chip chips-initial" >';
                             html+=respu[i].number_size+'-'+respu[i].name_size;
-                          html+='</div>';
+                          html+='</div><br><br>';
 					       
-					      html+='</div><div class="col s12 m12 l5 center-align stockec"><b>Stock: </b><i>'+ respu[i].tquantity+'</i><br><a href="javascript:void(0)" title="Incrementar" class="green waves-effect waves-green btn-flat" style="color:white"><i class="tiny material-icons">exposure_plus_1</i> </a> <label id="cantidad'+i+'">1</label>   <a href="javascript:void(0)" title="Descontar" class="red waves-effect waves-red btn-flat" style="color:white"><i class="tiny material-icons">exposure_neg_1</i></a></div>';
-					      html+='<div class="col s12 m12 l1 center-align">Precio <label class="price-new center-align">$'+respu[i].precio+'</label></div></div>';
+					      html+='</div><div class="col s12 m12 l3 center-align stockec"><b>Stock: </b><i>'+ respu[i].tquantity+'</i><br><a href="javascript:void(0)" title="Incrementar" class="btn green waves-effect waves-green" style="color:white" onClick="mas('+i+','+String("'"+respu[i].id_shp_c+"'")+','+String("'"+respu[i].id_prices+"'")+','+String("'"+respu[i].id_mat+"'")+','+String("'"+respu[i].id_color+"'")+','+String("'"+respu[i].id_size+"'")+','+String("'"+respu[i].tquantity+"'")+');"><i class="tiny material-icons">exposure_plus_1</i></a>&nbsp;&nbsp;<label style="font-size:17px;font-weight: bold;" id="cantidad'+i+'">1</label>&nbsp;&nbsp;<a href="javascript:void(0)" title="Descontar" class="btn red waves-effect waves-red" style="color:white" onClick="menos('+i+','+String("'"+respu[i].id_shp_c+"'")+','+String("'"+respu[i].id_prices+"'")+','+String("'"+respu[i].id_mat+"'")+','+String("'"+respu[i].id_color+"'")+','+String("'"+respu[i].id_size+"'")+','+String("'"+respu[i].tquantity+"'")+');"><i class="tiny material-icons">exposure_neg_1</i></a><br><br></div>';
+					      html+='<div class="col s12 m12 l1 center-align">Precio <label class="price-new right-align">$'+respu[i].precio+'</label><br><br></div><div class="col s12 m12 l2 center-align">Sub total<br><label class="right-align price-new">$0.00</label></div></div>';
 					    html+='</li> ';
 					    total+=parseFloat(respu[i].precio);
                 }
